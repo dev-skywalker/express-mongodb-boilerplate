afcabae6b6f313086ce0b03eb3edfb39a0fd13bd
Pyae Sone <pyaesone@Pyaes-MacBook-Air.local>
2022-08-07 12:19:53 +0630

    add product api

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,11 @@ import { createUserSchema } from "./schema/user.schema";
 import validateResource from "./middleware/validateResource";
 import { createUserHandler } from "./controller/user.controller";
 import {createSessionSchema} from "./schema/session.schema";
+import {createProductSchema, getProductSchema,deleteProductSchema, updateProductSchema} from "./schema/product.schema";
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
 import requireUser from "./middleware/requireUser";
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from "./controller/product.controller";
+
 
 function routes(app:Express){
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -16,6 +19,26 @@ function routes(app:Express){
     app.get("/api/sessions", requireUser, getUserSessionsHandler);
     
     app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+    app.post("/api/products",[requireUser, validateResource(createProductSchema)],createProductHandler);
+
+    app.put(
+        "/api/products/:productId",
+        [requireUser, validateResource(updateProductSchema)],
+        updateProductHandler
+      );
+    
+      app.get(
+        "/api/products/:productId",
+        validateResource(getProductSchema),
+        getProductHandler
+      );
+    
+      app.delete(
+        "/api/products/:productId",
+        [requireUser, validateResource(deleteProductSchema)],
+        deleteProductHandler
+      );
 }
 
 export default routes;
---
+++ src/services/product.service.ts
@@ -0,0 +1,55 @@
+import { DocumentDefinition, FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
+import ProductModel, {
+  ProductDocument,
+  ProductInput,
+} from "../models/product.model";
+
+export async function createProduct(input: DocumentDefinition<Omit<ProductDocument, "createdAt" | "updatedAt">>) {
+    return ProductModel.create(input);
+//   const metricsLabels = {
+//     operation: "createProduct",
+//   };
+
+  //const timer = databaseResponseTimeHistogram.startTimer();
+  //try {
+    //const result = await ProductModel.create(input);
+    //timer({ ...metricsLabels, success: "true" });
+    //return result;
+  //} catch (e) {
+    //timer({ ...metricsLabels, success: "false" });
+    //throw e;
+  //}
+}
+
+export async function findProduct(
+  query: FilterQuery<ProductDocument>,
+  options: QueryOptions = { lean: true }
+) {
+    return  ProductModel.findOne(query, {}, options);
+//   const metricsLabels = {
+//     operation: "findProduct",
+//   };
+
+  //const timer = databaseResponseTimeHistogram.startTimer();
+  //try {
+   //const result = await ProductModel.findOne(query, {}, options);
+    //timer({ ...metricsLabels, success: "true" });
+    //return result;
+  //} catch (e) {
+    //timer({ ...metricsLabels, success: "false" });
+
+    //throw e;
+  //}
+}
+
+export async function findAndUpdateProduct(
+  query: FilterQuery<ProductDocument>,
+  update: UpdateQuery<ProductDocument>,
+  options: QueryOptions
+) {
+  return ProductModel.findOneAndUpdate(query, update, options);
+}
+
+export async function deleteProduct(query: FilterQuery<ProductDocument>) {
+  return ProductModel.deleteOne(query);
+}
