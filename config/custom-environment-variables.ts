b7ad9c3e3de9401ff39cad3103e50fe23df7dcf6
Pyae Sone <pyaesone@Pyaes-MacBook-Air.local>
2022-08-08 15:54:50 +0630

    add env variable

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,10 @@
+export default {
+    accessTokenPrivateKey : 'ACCESS_TOKEN_PRIVATE_KEY',
+    accessTokenPublicKey : 'ACCESS_TOKEN_PUPLIC_KEY',
+    refreshTokenPrivateKey : 'REFRESH_TOKEN_PRIVATE_KEY',
+    refreshTokenPublicKey : 'REFRESH_TOKEN_PUPLIC_KEY',
+
+    port: 'PORT',
+    dbUri: 'DB_URI',
+    
+}
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import config from 'config';
 import connect from './utils/connect';
 import logger from './utils/logger';
 import createServer from "./utils/server";
 import swaggerDocs from "./utils/swagger";
 
+
 const port = config.get<number>('port');
+
+console.log(config.get('accessTokenPrivateKey'));
+console.log("ggg");
 
 const app = createServer();
 
